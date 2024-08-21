7af671d806b3ec62b6f183c3f17a5c2658fc902c
AhosanKabir <kaziahosan1998@gmail.com>
2024-08-21 21:00:49 +0600

    clouser added

--- Closures/Closures.js
+++ Closures/Closures.js
@@ -1,1 +1,38 @@
-console.log("Hello World");
+// basic practice without arguments------------->>
+function closuresDemoWithoutArguments(){
+    let x = 10;
+    return function(){
+        let y = 20;
+        console.log(`Sum: ${x + y}`);
+    }
+}
+closuresDemoWithoutArguments()();
+
+// basic practice with arguments --------------->>
+function closuresDemoWithArguments(x){
+    return function(y){
+        console.log(`Sum: ${x + y}`);
+    }
+}
+closuresDemoWithArguments(10)(30);
+
+// basic practice with Loops --------------->>
+for(let i = 0; i < 5; i++){
+    (function(val) {
+        setTimeout(()=>{
+            console.log(`values----- ${val}`);
+        }, 1000);
+    }(i));
+}
+
+
+
+// Problem 1 ----------------->>
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+let createHelloWorld = () => {
+    return function(){
+        return console.log("Hello World");
+    }
+}
+createHelloWorld()();
